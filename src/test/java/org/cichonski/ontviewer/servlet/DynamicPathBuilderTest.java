cf2df46ea37ebcfc965ab8ae4f7f0f6333675b28
Paul Cichonski <paul.cichonski@gmail.com>
2012-06-26 13:55:01 -0400

    test for buildingRootPath from DynamicPathBuilder.

--- src/test/java/org/cichonski/ontviewer/servlet/DynamicPathBuilderTest.java
+++ src/test/java/org/cichonski/ontviewer/servlet/DynamicPathBuilderTest.java
@@ -26,8 +26,31 @@ public class DynamicPathBuilderTest extends TestCase {
     
     public void testBuildRootPath(){
     	final DynamicPathBuilder pathBuilder = new DynamicPathBuilder(SERVLET_PATH);
+    	String fakeResource = "css/resource.css";
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../" + fakeResource);
+    	pathBuilder.pushLocalPath("local1");
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../" + fakeResource);
+    	pathBuilder.pushLocalPath("local2");
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../../" + fakeResource);
+    	pathBuilder.pushLocalPath("local3");
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../../../" + fakeResource);
+    	//now pop
+    	pathBuilder.popLocalPath();
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../../" + fakeResource);
+    	// push once more just for good measure
+    	pathBuilder.pushLocalPath("local3");
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../../../" + fakeResource);
+    	// pop all
+    	pathBuilder.popLocalPath();
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../../" + fakeResource);
+    	pathBuilder.popLocalPath();
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../../" + fakeResource);
+    	pathBuilder.popLocalPath();
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../" + fakeResource);
+    	pathBuilder.popLocalPath(); // shouldn't do anything
+    	assertEquals(pathBuilder.buildRootPath(fakeResource),  "../" + fakeResource);
     }
-    
+    	
     public void testBuildPath(){
     	
     }
